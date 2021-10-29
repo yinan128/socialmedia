b056b6b12d8f5e4d194c3a7cd82753d319777013
Minhao Jin <minhaoj@andrew.cmu.edu>
2021-10-28 21:03:38 -0400

    Add Google OAuth2

---
+++ socialmedia/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from socialmedia import views
+
+urlpatterns = [
+    path('', views.global_stream, name='global_stream'),
+    path('login', views.login_action, name='login'),
+]
--- socialmedia/views.py
+++ socialmedia/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='login')
 def global_stream(request):
+    print("here???")
     return render(request, 'socialmedia/index.html')
+
+def login_action(request):
+    return render(request, 'socialmedia/login.html')
